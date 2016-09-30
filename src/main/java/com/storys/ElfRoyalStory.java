c92b69e29090a4678f3efe97724de521a3e645b3
Forrest <forrest.corry@gmail.com>
2016-09-30 10:15:34 -0400

    initial story

---
+++ src/main/java/com/storys/ElfRoyalStory.java
@@ -0,0 +1,67 @@
+package com.storys;
+
+import com.interfaces.StoryInterface;
+
+public class ElfRoyalStory implements StoryInterface {
+
+	@Override
+	public void storyOne() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceOne() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceTwo() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceThree() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceOneA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceOneB() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceTwoA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceTwoB() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceThreeA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceThreeB() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/storys/TwilekJediStory.java
@@ -0,0 +1,73 @@
+package com.storys;
+
+import java.util.Scanner;
+
+import com.interfaces.StoryInterface;
+
+public class TwilekJediStory implements StoryInterface {
+	
+	Scanner sc = new Scanner(System.in);
+
+	@Override
+	public void storyOne() {
+		System.out.println("Path 1, 2, or 3");
+		
+		String input = sc.nextLine();
+
+	}
+
+	@Override
+	public void choiceOne() {
+		
+
+	}
+
+	@Override
+	public void choiceTwo() {
+		
+
+	}
+
+	@Override
+	public void choiceThree() {
+		
+
+	}
+
+	@Override
+	public void choiceOneA() {
+		
+
+	}
+
+	@Override
+	public void choiceOneB() {
+		
+
+	}
+
+	@Override
+	public void choiceTwoA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceTwoB() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceThreeA() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void choiceThreeB() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
